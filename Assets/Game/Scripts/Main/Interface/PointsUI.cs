b860e8eea1087c9b5a48eec3790d87e4d91e3afd
DOG007ua <dog007ua@gmail.com>
2022-02-19 20:10:32 +0200

    Create points UI

--- Assets/Game/Scripts/Main/Interface/PointsUI.cs
+++ Assets/Game/Scripts/Main/Interface/PointsUI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -8,28 +9,69 @@ namespace Game.Scripts.Main.Interface
 {
     public class PointsUI : MonoBehaviour, IPointsUI
     {
-        [SerializeField] private Text textComponent;
-        private float normalSizeFont;
-        private float coefSizeFont = 1.2f;
-        private float timeScale = 0.5f;
+        [SerializeField] private Text textComponentPoints;
+        [SerializeField] private Text textComponentPointsChange;
+        private int normalSizeFont;
+        private int maxSizeFont;
+        private float timeScale = 0.35f;
         private bool isScale;
+        private int oldPoints;
         
         public void UpdatePoints(float value)
         {
-            textComponent.text = ((int)value).ToString();
+            var points = (int)value;
+            textComponentPoints.text = points.ToString();
+            normalSizeFont = textComponentPoints.fontSize;
+            maxSizeFont = (int)(normalSizeFont * 1.4f);
+            WriteChangePoints(points - oldPoints);
+            oldPoints = points;
+            Scale();
         }
-
-        private void Update()
-        {
-            if (isScale)
-            {
-                   
-            }
-        }
-
+        
         private void Start()
         {
-            DOTween
+            
+        }
+
+        private void Scale()
+        {
+            DOTween.Sequence()
+                .AppendCallback(() => ScaleAnimation(maxSizeFont))
+                .AppendInterval(timeScale / 2.0f)
+                .AppendCallback(() => ScaleAnimation(normalSizeFont));
+        }
+        
+        private void ChangeColor()
+        {
+            var time = 0.4f;
+            DOTween.Sequence()
+                .AppendCallback(() => ColorChangePoint(1, time))
+                .AppendInterval(0.4f)
+                .AppendCallback(() => ColorChangePoint(0, time));
+        }
+
+        private void ScaleAnimation(int value)
+        {
+            DOTween.To(() => textComponentPoints.fontSize, x => textComponentPoints.fontSize = x, value,  timeScale / 2.0f);
+        }
+
+        void WriteChangePoints(int points)
+        {
+            var text = points > 0 ? $"+{points}" : points.ToString();
+            textComponentPointsChange.text = text;
+            ChangeColor();
+        }
+        
+        private void ColorChangePoint(int value, float time)
+        {
+            DOTween.To(() => textComponentPointsChange.color.a, ChangeColor, value,  time);
+        }
+
+        private void ChangeColor(float value)
+        {
+            var color = textComponentPointsChange.color;
+            color.a = value;
+            textComponentPointsChange.color = color;
         }
     }
 }
